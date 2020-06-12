feea495560bed9994aa5d5c6154f047f4dedf1b3
robertbutacu <robertbutacu@gmail.com>
2020-06-12 11:33:22 +0100

    Indentation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,19 +9,24 @@ import scala.concurrent.ExecutionContext.Implicits
 object Main extends IOApp {
   def time[A](f: => IO[A]): IO[A] = {
     for {
-      start <- IO(Instant.now)
-      _ <- IO(println("Starting computation..."))
-      result <- f
-      end <- IO(Instant.now)
+      start      <- IO(Instant.now)
+      _          <- IO(println("Starting computation..."))
+      result     <- f
+      end        <- IO(Instant.now)
       difference <- IO(end.toEpochMilli - start.toEpochMilli)
-     _ <- IO(println(TimeUnit.MILLISECONDS.toSeconds(difference)))
+      _          <- IO(println(TimeUnit.MILLISECONDS.toSeconds(difference)))
     } yield result
   }
 
   override def run(args: List[String]): IO[ExitCode] = {
     val blocker = Blocker.liftExecutionContext(Implicits.global)
-    time(FastHash.hash(new File("/Users/robertbutacu/pet-projects/fs2-fast-md5/Docker.dmg"))(blocker)).flatMap {
-      result => IO(println(s"MD5 outcome is $result"))
-    }.as(ExitCode.Success)
+    time(
+      FastHash.hash(
+        new File("/Users/robertbutacu/pet-projects/fs2-fast-md5/Docker.dmg")
+      )(blocker)
+    ).flatMap { result =>
+        IO(println(s"MD5 outcome is $result"))
+      }
+      .as(ExitCode.Success)
   }
 }
